a004da5fdd98eb429c09360d40452553297ad7e0
mtechkiran <mtech.kiran@gmail.com>
2025-04-07 20:15:29 +0530

    Initial Commit

---
+++ src/main/java/com/mypc/demoMaven/App.java
@@ -0,0 +1,10 @@
+package com.mypc.demoMaven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Welcome to Azure Devops");
+    }
+}
